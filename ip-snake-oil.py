d4668b4693faa478b02183b87127bfe1bf7feba6
molo976 <molo976@gmail.com>
2017-09-05 16:48:48 +0100

    moving into working out number of hosts

--- ip-snake-oil.py
+++ ip-snake-oil.py
@@ -48,3 +48,45 @@ network_bits = int(mask) * '1'
 
 # Get the host bits portion of the mask:
 host_bits = (32 - int(mask)) * '0'
+
+# Create the whole mask in binary:
+mask_bits = network_bits + host_bits
+
+# Now cut into 4 octets and turn into binary (not actually used anywhere in the script at present, so commented out)
+#mask_binary_octets = []
+#mask_binary_octets.append(mask_bits[0:7])
+#mask_binary_octets.append(mask_bits[8:15])
+#mask_binary_octets.append(mask_bits[16:23])
+#mask_binary_octets.append(mask_bits[24:31])
+
+# Create dotted decimal subnet mask string
+subnet_mask = str(int(mask_bits[0:8], 2)) + '.' + str(int(mask_bits[8:16], 2)) + '.' + str(int(mask_bits[16:24], 2)) + '.' + str(int(mask_bits[24:32], 2))
+
+print('The subnet mask is:')
+print(subnet_mask)
+
+
+# Now need to find the first & last host plus broadcast address. 
+
+# Turn host_bits (current binary zeros) to binary 1's for calculating number of available host IPs
+# Join found from StackOverflow - need to spend some more time learning this useful tool 
+# Explanation & examples here: https://www.tutorialspoint.com/python/string_join.htm
+host_bits_inverted = ''.join('1' if x == '0' else '0' for x in host_bits)
+
+
+# Some rough stuff here direct from idle, gotta run, will return:
+>>> subnet_mask
+'255.255.255.128'
+>>> subnet_octets = subnet_mask.split('.')
+>>> subnet_octets
+['255', '255', '255', '128']
+>>> for octet in subnet_octets;
+  File "<stdin>", line 1
+    for octet in subnet_octets;
+                              ^
+SyntaxError: invalid syntax
+>>> for octet in subnet_octets:
+...     x =+ 1
+...     if octet != '255'
+
+
