cd20c1fa065bb129eed6788c6ee8c3f47a71aabe
molo976 <molo976@gmail.com>
2017-09-07 16:41:52 +0100

    now have number of hosts ips in subnet

--- ip-snake-oil.py
+++ ip-snake-oil.py
@@ -26,22 +26,7 @@ for octet in binary_octets:
 # Now the ip address is stored as 4 integers in the octets list, as 4 binary numbers in the binary_octets list, and as a whole binary string in the binary_whole_ip variable:
 
 print('\n')
-print('The IP entered was:')
-print(ip)
-print('\n')
-print('The IP when converted to four octets is:')
-print(octets)
-print('\n')
-print('The IP when converted into four binary objects is')
-print(binary_octets)
-print('\n')
-print('When prepended with zeros so each octet is 8 chars long')
-for octet in binary_octets:
-    print(str(octet).zfill(8))
-print('\n')
-print('And finally the whole string in binary')
-print(binary_whole_ip)
-print('\n')
+print('The IP entered was:\t\t\t\t' + ip)
 
 # Moving onto the subnet mask, get the 'network bits' portion of the mask:
 network_bits = int(mask) * '1'
@@ -62,31 +47,21 @@ mask_bits = network_bits + host_bits
 # Create dotted decimal subnet mask string
 subnet_mask = str(int(mask_bits[0:8], 2)) + '.' + str(int(mask_bits[8:16], 2)) + '.' + str(int(mask_bits[16:24], 2)) + '.' + str(int(mask_bits[24:32], 2))
 
-print('The subnet mask is:')
-print(subnet_mask)
-
+print('The subnet mask is:\t\t\t\t' + subnet_mask)
 
 # Now need to find the first & last host plus broadcast address. 
 
 # Turn host_bits (current binary zeros) to binary 1's for calculating number of available host IPs
 # Join found from StackOverflow - need to spend some more time learning this useful tool 
 # Explanation & examples here: https://www.tutorialspoint.com/python/string_join.htm
-host_bits_inverted = ''.join('1' if x == '0' else '0' for x in host_bits)
+# Found that I don't actually need this to work out the number of hosts. 
+# host_bits_inverted = ''.join('1' if x == '0' else '0' for x in host_bits)
 
 
-# Some rough stuff here direct from idle, gotta run, will return:
->>> subnet_mask
-'255.255.255.128'
->>> subnet_octets = subnet_mask.split('.')
->>> subnet_octets
-['255', '255', '255', '128']
->>> for octet in subnet_octets;
-  File "<stdin>", line 1
-    for octet in subnet_octets;
-                              ^
-SyntaxError: invalid syntax
->>> for octet in subnet_octets:
-...     x =+ 1
-...     if octet != '255'
+# Print the number of host IP's available:
+host_network_mask = '1' + host_bits
+print('Number of IPs in the subnet:\t\t\t' + str(int(host_network_mask, 2)))
+print('Number of host IPs available in this network:\t' + str(int(host_network_mask, 2) - 2))
+
 
 
